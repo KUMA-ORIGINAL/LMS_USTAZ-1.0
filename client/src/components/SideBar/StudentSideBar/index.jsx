1f1f0fb1ee1d9ef276db6d631503c9cd25f53597
KanybekovMaksat <knb.max004@gmail.com>
2023-09-15 10:37:11 +0600

    nav

--- client/src/components/SideBar/StudentSideBar/index.jsx
+++ client/src/components/SideBar/StudentSideBar/index.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import BookIcon from "../../../assets/images/book-icon.png"
+import BookActiveIcon from "../../../assets/images/bookactive-icon.png"
+import RatingIcon from "../../../assets/images/rating-icon.png"
+import RatingActiveIcon from "../../../assets/images/ratingactive-icon.png"
+
+
 import { Link } from 'react-router-dom'
 
 const arrayForStudents = [
@@ -14,16 +19,30 @@ const arrayForStudents = [
 
 const StudentSideBar = () => {
   return (
-    <>
-      {arrayForStudents.map((item) => {
+    <div className="aside__row">
+      {/* {arrayForStudents.map((item) => {
         return (
           <Link to={item.link} className="aside__item">
-            <span className="aside__title">{item.title}</span>
+            <div className="aside__title">
+              {item.title}
+              </div>
             <span className="aside__arrow"> {'>'} </span>
           </Link>
         )
-      })}
-    </>
+      })} */}
+      <div className="aside__block">
+      <Link to="/course/lecture" className="aside__item"> 
+      <img src={true ? BookActiveIcon : BookIcon} alt="" className="aside__item-img" />
+      Курсы
+      </Link>
+      </div>
+      <div className="aside__block">
+      <Link to="/course/lecture" className="aside__item"> 
+      <img src={true ? RatingActiveIcon : RatingIcon} alt="" className="aside__item-img" />
+      Оценки
+      </Link>
+      </div>
+    </div>
   )
 }
 
