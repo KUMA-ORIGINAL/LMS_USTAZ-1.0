b98552be2e5d95814c28cdaaa6b198e8e5febb98
KanybekovMaksat <knb.max004@gmail.com>
2023-10-11 00:59:05 +0600

    intro

--- client/src/pages/Introduction/index.jsx
+++ client/src/pages/Introduction/index.jsx
@@ -3,13 +3,16 @@ import Advantages from "./components/Advantages";
 import Courses from './components/Courses';
 import Footer from "./components/Footer";
 import Application from "./components/Application";
-
+import Metods from "./components/Metods"
+import Accordeon from './components/Accordeon'
 const Introduction = () => {
   return (
     <div >
       <Header/>
       <Advantages/>
       <Courses/>
+      <Metods/>
+      <Accordeon/>
       <Application/>
       <Footer/>
     </div>
