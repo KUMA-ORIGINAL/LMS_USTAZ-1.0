3c49458e0576247d3080a133fef3ceb013b0a47b
Kuma Original <Kuma_Original@gmail.com>
2023-10-26 17:24:26 +0600

    add urls serializers views in course app

--- server/course/urls.py
+++ server/course/urls.py
@@ -1,1 +1,14 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import CourseView, ModuleView, ContentView, ExamView
+
+router = routers.DefaultRouter()
+router.register(r'module', ModuleView, basename='module')
+router.register(r'content', ContentView, basename='content')
+router.register(r'exam', ExamView, basename='exam')
+router.register('', CourseView, basename='course')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- server/course/views.py
+++ server/course/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from account.permissions import IsAdminOrMentorPermission
+from course.models import Course, Module, Content, Exam
+from course.serializers import CourseSerializer, ModuleSerializer, ContentSerializer
+
+
+class CourseView(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrMentorPermission]
+
+
+class ModuleView(viewsets.ModelViewSet):
+    queryset = Module.objects.all()
+    serializer_class = ModuleSerializer
+    permission_classes = [IsAdminOrMentorPermission]
+
+
+class ContentView(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+    permission_classes = [IsAdminOrMentorPermission]
+
+
+class ExamView(viewsets.ModelViewSet):
+    queryset = Exam.objects.all()
+    serializer_class = ContentSerializer
+    permission_classes = [IsAdminOrMentorPermission]
