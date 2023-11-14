7dda2c1618d3094b5f317f02761ba99fee43441c
ma1abakiev <malabakievramzan@gmail.com>
2023-11-14 13:01:37 +0600

    demo day

--- client/src/pages/Mentor/StudentsList/components/List.jsx
+++ client/src/pages/Mentor/StudentsList/components/List.jsx
@@ -1,49 +1,54 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 const List = ({ students }) => {
   return (
     <StyledTable>
       <thead>
         <tr>
-        <th>№</th>
+          <th>№</th>
           <th>Фамилия</th>
           <th>Имя</th>
           <th>Telegram</th>
           <th>Email</th>
+          <th>Номер Телефона</th>
+          <th>Рейтинг</th>
+          <th>Баллы</th>
         </tr>
       </thead>
       <tbody>
         {students.map((student, index) => (
           <tr key={index}>
-            <td>{index+1}</td>
+            <td>{index + 1}</td>
             <td>{student.lastName}</td>
             <td>{student.firstName}</td>
             <td>{student.telegram}</td>
             <td>{student.email}</td>
+            <td>{student.tel}</td>
+            <td>{student.raiting}</td>
+            <td>{student.coins}</td>
           </tr>
         ))}
       </tbody>
     </StyledTable>
-  );
-};
+  )
+}
 
-export default List;
-
+export default List
 
 //styles for this components
 
 const StyledTable = styled.table`
-  width:100%;
-  border-collapse:collapse;
+  width: 100%;
+  border-collapse: collapse;
 
-  th, td{
-    border:1px solid;
-    padding:10px;
+  th,
+  td {
+    border: 1px solid;
+    padding: 10px;
   }
-  
-  td:first-child{
-    max-width:50px;
+
+  td:first-child {
+    max-width: 50px;
   }
 
   tr:hover {
--- client/src/pages/Mentor/StudentsList/index.jsx
+++ client/src/pages/Mentor/StudentsList/index.jsx
@@ -1,9 +1,7 @@
 //import components
-import List from './components/List';
-
+import List from './components/List'
 
 const MentorStudentsList = () => {
-
   //fetch students
   const students = [
     {
@@ -11,74 +9,105 @@ const MentorStudentsList = () => {
       firstName: 'Иван',
       telegram: '@kany_maks',
       email: 'maksat@mail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Петров',
       firstName: 'Петр',
       telegram: '@maxknb',
       email: 'maksatkanybekov2004_kg@gmail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Иванов',
       firstName: 'Иван',
       telegram: '@ivan123',
       email: 'ivan123@example.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Смирнова',
       firstName: 'Анна',
       telegram: '@annasmirnova',
       email: 'anna.smirnova@mail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Козлов',
       firstName: 'Дмитрий',
       telegram: '@dima_kozlov',
       email: 'dima.kozlov@example.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Соколова',
       firstName: 'Мария',
       telegram: '@maria_sokolova',
       email: 'maria.sokolova@mail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Иванова',
       firstName: 'Екатерина',
       telegram: '@katya_ivanova',
       email: 'katya.ivanova@example.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Кузнецов',
       firstName: 'Алексей',
       telegram: '@alex_kuznetsov',
       email: 'alex.kuznetsov@mail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Семенов',
       firstName: 'Андрей',
       telegram: '@andrey_semenov',
       email: 'andrey.semenov@example.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Павлова',
       firstName: 'Елена',
       telegram: '@elena_pavlova',
       email: 'elena.pavlova@mail.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
     {
       lastName: 'Михайлов',
       firstName: 'Сергей',
       telegram: '@sergey_mikhailov',
       email: 'sergey.mikhailov@example.com',
+      tel: '+996708186699',
+      raiting: '51.8%',
+      coins: 41,
     },
-
-
-  ];
+  ]
 
   return (
-    <div className='content'> 
-      <List students={students}/>
+    <div className="content">
+      <List students={students} />
     </div>
   )
 }
